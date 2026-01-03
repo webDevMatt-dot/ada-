2a971fe753f6e3c31aa407acb610b75e6bfeccc5
Matthew Gift Luis <matt@Matthews-MacBook-Pro.local>
2026-01-03 14:35:09 +0200

    check

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:8000'}/api/:path*/`,
+        destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:8000'}/api/:path*`,
       },
     ];
   },
